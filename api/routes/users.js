910f49feab8997344a8be75f051d06b4ce4eec62
ibrahim <ibrahim.bekkari@student.vinci.be>
2023-11-22 00:52:57 +0100

    add: loginPage - registerPage

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,4 +7,14 @@ router.get('/', (req, res) => {
   res.json({ users: [{ name: 'e-baron' }] });
 });
 
+/* test */
+router.post('/login', (req, res) => {
+  res.status(200).json({ message: 'Connexion réussie login' });
+});
+
+/* test */
+router.post('/register', (req, res) => {
+  res.status(200).json({ message: 'Connexion réussie register' });
+});
+
 module.exports = router;
