57e29892d561ba51e53f6d80a62c941c525179a8
Eda Copur <eda.copur@student.vinci.be>
2023-11-22 01:01:29 +0100

    add content to AboutUs + css modified

--- frontend/src/Components/Pages/AboutUsPage.js
+++ frontend/src/Components/Pages/AboutUsPage.js
@@ -4,6 +4,56 @@ import { clearPage } from '../../utils/render';
 const aboutUs = () => {
   clearPage();
   renderGoBackHomeButton();
+
+  const main = document.querySelector('main');
+  main.innerHTML = `
+  <section id="SectionMain">
+
+  <section class="banner pt-5 d-flex justify-content-center align-items-center ">
+    <div class="container my-5 py-5">
+      <div class="row">
+      <div class="col-md-6 text-center">
+      <h1 class="titleAU"> L'EQUIPE </h1>
+          </div>
+          <div class="col-md-6 text-center">
+          <h2 class="textAU"> Nous sommes cinq étudiants en deuxième année en informatique
+          spécialisés dans le développement d'applications à la Haute Ecole Léonard de Vinci. <h2>
+          </div>
+ 
+          </div>
+ 
+
+ 
+
+  </section>
+
+  <section class="banner pt-5 d-flex justify-content-center align-items-center ">
+  <div class="container my-5 py-5">
+    <div class="row">
+      <div class="col-md-6 text-center order-md-1">
+      <h1 class="titleAU"> NOTRE PROJET </h1>
+      </div>
+      <div class="col-md-6 text-center order-md-2">
+      <h2 class="textAU" >Dans le cadre de notre cours, nous avons conçu et développé un jeu de quiz éducatif. 
+      Lors cette expérience interactive, les utilisateurs peuvent choisir parmi des catégories prédéfinies ou même 
+      créer leurs propres quiz. Qu'ils préfèrent tester leurs connaissances ou défier leurs amis, notre plateforme 
+      offre un éventail d'options de quiz passionnants.
+      À la fin de chaque quiz, les joueurs reçoivent un score qui reflète leur performance, 
+      et ils ont également la possibilité de gagner une collection de badges. 
+      Ces badges sont des récompenses que les joueurs peuvent accumuler en participant activement au jeu. </h2>
+    
+        
+    
+      </div>
+    </div>
+  </div>
+</section>
+
+
+  <h1 class="titleAU"> CONTACTER NOUS </h1>
+
+
+  </section>`;
 };
 
 function renderGoBackHomeButton() {
