108025b29fda40f2e44af91e915f0d7794a4b141
hammouten <asmae.hammouten@student.vinci.be>
2023-11-22 18:33:22 +0100

    modify css login

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -5,34 +5,33 @@ import { clearPage } from '../../utils/render';
 function renderLoginForm() {
   const main = document.querySelector('main');
 
-  main.innerHTML = 
-  `
+  main.innerHTML = `
   <div id = "containerAuthentification" class="container-xxl d-flex justify-content-center align-items-center pt-5">
     <div id = "squareLogin" class="w-75">
-    <div class="card shadow-lg ">
-    <div class="card-body p-5">
-    <h2 class="fs-4 card-title fw-bold mb-4 text-center">Connexion</h2>
-      <form>
-      <div class="mb-3">
-      <label class="mb-2 text-muted" for="email">Pseudo</label>
-      <input id="username" type="text" class="form-control" name="email" value="" required autofocus placeholder = "baron12"/>
-    </div>
+      <div class="card shadow-lg ">
+        <div class="card-body p-5">
+          <h2 class="fs-4 card-title fw-bold mb-4 text-center">Connexion</h2>
+          <form>
+          <div class="mb-3">
+            <label class="mb-2 text-muted" for="email">Pseudo</label>
+            <input id="username" type="text" class="form-control" name="email" value="" required autofocus placeholder = "baron12"/>
+          </div>
 
-    <div class="mb-3">
-    <div class="mb-2 w-100">
-      <label class="text-muted" for="password">Mot de passe</label>
-    </div>
-    <input id="password" type="password" class="form-control" name="password" required placeholder = "••••••••"/>
-  </div>
+        <div class="mb-3">
+          <div class="mb-2 w-100">
+          <label class="text-muted" for="password">Mot de passe</label>
+        </div>
+      <input id="password" type="password" class="form-control" name="password" required placeholder = "••••••••"/>
+      </div>
 
-  <div class="mb-3 form-check">
+      <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="rememberMe">
-        <label class="form-check-label" for="rememberMe"></label>
+        <label class="form-check-label" for="rememberMe">Se souvenir de moi</label>
       </div>
 
       <div class="mb-3">
       <input id="login" type="button" class="btn btn-stylem mn-3  w-100" value="Se connecter" />
-    </div>
+      </div>
 
     <hr>
 
