c030a77b50032871cd9b74fbc54d0b06cf78ab72
dania <dania.ouarrad@student.vinci.be>
2023-11-24 19:35:32 +0100

    fix : homePage

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -12,49 +12,46 @@ const HomePage = () => {
   main.innerHTML = `
   <section id="SectionMain">
 
-  <section class="banner pt-5 d-flex justify-content-center align-items-center ">
-    <div class="container my-5 py-5">
-      <div class="row">
-      <div class="col-md-12 col-12 col-lg-6 text-center d-flex align-items-center">
-          <h1  >
-          Prêt à explorer l'univers des quiz ? Découvrez une multitude de catégories et mettez vos connaissances à l'épreuve !
-          <div>
-          <a class="btn btn-style" href="/categories" data-uri="">Explorer les catégories</a> <!--href ou data-uri????-->
+        <section class="banner pt-5 d-flex justify-content-center align-items-center ">
+          <div class="container my-5 py-5">
+            <div class="row">
+            <div class="col-md-12 col-12 col-lg-6 text-center d-flex align-items-center">
+                <h1  >
+                Prêt à explorer l'univers des quiz ? Découvrez une multitude de catégories et mettez vos connaissances à l'épreuve !
+                <div>
+                <a class="btn btn-style" href="/categories" data-uri="">Explorer les catégories</a> <!--href ou data-uri????-->
+                </div>
+                </h1>
+                </div>
+                <div class="col-12 col-lg-6 col-md-12">
+                <img  class="img-fluid" src="${image1}" alt="image banner">
+                </div>
+                </div>
           </div>
-          </h1>
-          </div>
+        </section>
+      
+        <svg style="background color:#D9EEE1;" width="100%" height="70" viewBox="0 0 100 100"' preserveAspectRatio="none"> 
+        <path id="wavepath" d="M0, 0 L110, 0C35,150 35,0 0,100z" fill="#F4EEFF"></path>
+         </svg>
+      
+        <section class="pt-5 d-flex justify-content-center align-items-center ">
+        <div class="container my-5 py-5">
+          <div class="row">
           <div class="col-12 col-lg-6 col-md-12">
-          <img  class="img-fluid" src="${image1}" alt="imagePInterro">
+          <h1> <img class="img-fluid"src="${image2}" alt="image banner"> <h1> 
+            </div>
+            <div class="col-md-12 col-12 col-lg-6 text-center d-flex align-items-center">
+            <h3>
+            Partager vos connaissances avec le monde ?<br>Laissez-vous emporter par votre imagination et créez votre propre QuizWiz !
+            <div>
+            <a class="btn btn-style" href="/register" data-uri="">Inscrivez vous gratuitement</a> <!--href ou data-uri????-->
+            </div>
+              </h3>
+              </div>
+            </div>
           </div>
-          </div>
-    </div>
-  </section>
-
-
-  <svg style="background color:#D9EEE1;" width="100%" height="70" viewBox="0 0 100 100"' preserveAspectRatio="none"> 
-  <path id="wavepath" d="M0, 0 L110, 0C35,150 35,0 0,100z" fill="#F4EEFF"></path>
-   </svg>
-
-  <section class="pt-5 d-flex justify-content-center align-items-center ">
-  <div class="container my-5 py-5">
-    <div class="row">
-    <div class="col-12 col-lg-6 col-md-12">
-    <h1> <img class="img-fluid"src="${image2}" alt="imageCreation"> <h1> 
-      </div>
-      <div class="col-md-12 col-12 col-lg-6 text-center d-flex align-items-center">
-      <h3>
-      Partager vos connaissances avec le monde ?<br>Laissez-vous emporter par votre imagination et créez votre propre QuizWiz !
-      <div>
-      <a class="btn btn-style" href="/categories" data-uri="">Inscrivez vous gratuitement</a> <!--href ou data-uri????-->
-      </div>
-        </h3>
-        </div>
-      </div>
-    </div>
-</section>
-
-  </section>`;
+      </section>
+        </section>`;
 };
- 
+
 export default HomePage;
- 
