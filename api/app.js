ad0059c6ddc1337ebcf4c388b144cc2dc2dc9ade
ibrahim <ibrahim.bekkari@student.vinci.be>
2023-11-24 16:53:42 +0100

    refactor data setup up in app.js

--- api/app.js
+++ api/app.js
@@ -3,23 +3,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const pg = require('pg');
-
-// eslint-disable-next-line no-trailing-spaces
-// Remplacez la ligne User & default database par celui de elephantsql 
-const conString = 'postgres://btuuybgk:1IQDmJBcwY7B0AXs-mgMj1GNum5WOdmn@ella.db.elephantsql.com/btuuybgk';
-const client = new pg.Client(conString);
-// eslint-disable-next-line max-len
-// eslint-disable-next-line prefer-arrow-callback, func-names, consistent-return, space-before-function-paren
-client.connect(function(err) {
-  if (err) {
-    // eslint-disable-next-line no-console
-    return console.error('Could not connect to PostgreSQL', err);
-  }
-  // eslint-disable-next-line no-console
-  console.log('Connected to PostgreSQL database');
-});
-
 const corsOptions = {
   origin: ['http://localhost:8080', 'https://e-baron.github.io'],
 };
