f7fbc1b1d09b64920722c5932984fa33de16bfdf
dania <dania.ouarrad@student.vinci.be>
2023-11-26 12:34:48 +0100

    add: addOneQuiz function for creating quizzes

--- frontend/src/models/quizzes.js
+++ frontend/src/models/quizzes.js
@@ -1,26 +1,55 @@
 /* eslint-disable no-console */
 const readAllCategories = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/quizzes/categories');
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP: ${response.status}`);
-      }
-      // const result = await response.json();
-      const categories = await response.json();
-      console.log('Categories :', categories);
-      return categories;
-    } catch (error) {
-      console.error(error.message);
-      throw error;
+  try {
+    const response = await fetch('http://localhost:3000/quizzes/categories');
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP: ${response.status}`);
     }
-  };
+    // const result = await response.json();
+    const categories = await response.json();
+    console.log('Categories :', categories);
+    return categories;
+  } catch (err) {
+    console.error('readAllCategories::error: ', err);
+    throw err;
+  }
+};
 
-  // eslint-disable-next-line no-unused-vars
-  const addOneQuiz = async (quiz) => {
-    // 
-  };
-  
-  // eslint-disable-next-line import/prefer-default-export
-  export { readAllCategories, addOneQuiz };
+const addOneQuiz = async (quiz) => {
+  console.log('I am in models/quizzes.js, in the function addOneQuiz');
+  console.log('param quiz : ', quiz);
 
-  
+  const main = document.querySelector('main');
+  main.innerHTML = `
+  <div class="text-center" id="loadingSpinner" style="display: none;">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+`;
+  const loadingSpinner = document.querySelector('#loadingSpinner');
+  try {
+    loadingSpinner.style.display = 'block';
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(quiz),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch('http://localhost:3000/quizzes', options);
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP: ${response.status}`);
+    }
+    loadingSpinner.style.display = 'none'; 
+    const createdQuiz = await response.json();
+    console.log('createdQuiz :', createdQuiz);
+    return createdQuiz;
+  } catch (err) {
+    loadingSpinner.style.display = 'none'; 
+    console.error('addOneQuiz::error: ', err);
+    throw err;
+  }
+};
+
+export { readAllCategories, addOneQuiz };
