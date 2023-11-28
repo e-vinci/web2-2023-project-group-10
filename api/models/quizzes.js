2a0df49fa963b579ab495cf0082658fcdc3a42f4
dania <dania.ouarrad@student.vinci.be>
2023-11-28 19:11:52 +0100

    Add functions for quiz creation and retrieval in models/quizzes.js

--- api/models/quizzes.js
+++ api/models/quizzes.js
@@ -1,16 +1,105 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable consistent-return */
 const pool = require('../db');
 
+/**
+ * Retrieves all quizzes for a user based on the user's ID.
+ * If quizzes are found, returns an array of quiz objects.
+ * If no quizzes are found, returns undefined.
+ * userId : The user's ID.
+ */
 async function readAllQuizzesByUser(userId) {
   const quizzes = await pool.query('SELECT * FROM project.quizzes WHERE user_id=$1', [userId]);
   if (quizzes.rows.length > 0) {
-    // console.log('quizzes ok');
+    console.log('quizzes ok');
     return quizzes.rows;
   }
   return undefined;
 }
 
+/**
+ * Retrieves a category based on its label.
+ * If a category is found, returns an array with the category object.
+ * If no category is found, returns undefined.
+ * label : The label of the category.
+ */
+async function readCategoryByLabel(label) {
+  const category = await pool.query('SELECT * FROM project.categories WHERE label = $1', [label]);
+  if (category.rows.length > 0) {
+    console.log(`Category ${label} retrieved successfully.`);
+    return category.rows;
+  }
+  return undefined;
+}
+
+/**
+ * Retrieves all categories from the database.
+ * If categories are found, returns an array of category objects.
+ * If no categories are found, returns undefined.
+ */
+async function readAllCategories() {
+  const categories = await pool.query('SELECT * FROM project.categories');
+  if (categories.rows.length > 0) {
+    console.log('Categories retrieved successfully.');
+    return categories.rows;
+  }
+  return undefined;
+}
+
+async function addOneQuiz(categoryId, title, user) {
+  const quiz = await pool.query(
+    'INSERT INTO project.quizzes (user_id, category, title) VALUES ($1, $2, $3) RETURNING  quiz_id, title, category',
+    [user, categoryId, title],
+  );
+  if (quiz.rows.length > 0) {
+    console.log('Quiz add successfully.');
+    return quiz.rows;
+  }
+  return undefined;
+}
+
+async function addQuestionsAnswers(questions, quizId) {
+  console.log('questions.lenght : ', questions.length);
+  for (let index = 0; index < questions.length; index++) {
+    console.log('the question is : ', questions[index][0]);
+    const question = await pool.query(
+      'INSERT INTO project.questions (quiz_id, question) VALUES ($1, $2) RETURNING  question_id ',
+      [quizId, questions[index][0]],
+    );
+    if (question.rowCount <= 0) {
+      return undefined;
+    }
+    const questionId = question.rows[0].question_id;
+    // add the answers to the question table :
+    for (let j = 1; j <= 4; j++) {
+      if (j === 1) {
+        const answer = await pool.query(
+          'INSERT INTO project.answers (answer, question, is_correct) VALUES ($1, $2, $3) RETURNING  answer ',
+          [questions[index][j], questionId, 1],
+        );
+        if (answer.rowCount <= 0) {
+          return undefined;
+        }
+      } else {
+        const answer = await pool.query(
+          'INSERT INTO project.answers (answer, question, is_correct) VALUES ($1, $2, $3) RETURNING  answer ',
+          [questions[index][j], questionId, 0],
+        );
+        if (answer.rowCount <= 0) {
+          return undefined;
+        }
+      }
+    }
+  }
+  return { message: 'Questions and answers added successfully.' };
+}
 module.exports = {
   readAllQuizzesByUser,
+  readCategoryByLabel,
+  readAllCategories,
+  addOneQuiz,
+  addQuestionsAnswers,
 };
--- api/routes/quizzes.js
+++ api/routes/quizzes.js
@@ -4,16 +4,25 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-console */
 const express = require('express');
-const pool = require('../db');
 
 const router = express.Router();
-const { readAllQuizzesByUser } = require('../models/quizzes');
 
+const {
+  readAllQuizzesByUser,
+  readAllCategories,
+  readCategoryByLabel,
+  addOneQuiz,
+  addQuestionsAnswers,
+} = require('../models/quizzes');
+
+/**
+ *  Return all quizzes for a user.
+ *  user-id: The user's ID passed as a query parameter.
+ */
 router.get('/', async (req, res) => {
   const userId = req?.query ? Number(req.query['user-id']) : undefined;
   try {
     const quizzes = await readAllQuizzesByUser(userId);
-    // quand on appel une focntion asyn, obliger de mettre await ici !!
     console.log(quizzes);
     if (quizzes !== undefined) return res.json(quizzes);
     return res.sendStatus(400);
@@ -23,98 +32,52 @@ router.get('/', async (req, res) => {
   }
 });
 
+/**
+ * Create a new quiz.
+ * title : The title of the quiz.
+ * category : The category label for the quiz.
+ * questions : An array of questions and their corresponding answers.
+ */
 router.post('/', async (req, res) => {
   console.log('POST routes/quizzes');
   const { title, category, questions } = req.body;
   try {
     // recover the selected category
-    const resCategory = await pool.query('SELECT * FROM project.categories WHERE label = $1', [
-      category,
-    ]);
-    const selectedCategory = resCategory.rows[0];
-    if (!selectedCategory) {
-      return res.status(404).json({ message: 'Catégorie non trouvée' });
+    const categorySelected = await readCategoryByLabel(category);
+    if (!categorySelected) {
+      return res.status(404).json({ message: 'Category not found' });
     }
-    const categoryId = selectedCategory.category_id;
+    const categoryId = categorySelected[0].category_id;
     console.log(`id category :  ${categoryId}`);
-
     // add the quiz to the quiz table
-    const quiz = await pool.query(
-      'INSERT INTO project.quizzes (user_id, category, title) VALUES ($1, $2, $3) RETURNING  quiz_id, title, category',
-      [6, categoryId, title], // a changer par l'id de 'lutilisateur courant !!
-    );
-    const quizId = quiz.rows[0].quiz_id;
-    console.log("Le quiz créé à l'id :", quizId);
-    if (quiz.rowCount <= 0) {
-      res.status(400).send('Erreur lors de l’enregistrement du quiz');
+    const quiz = await addOneQuiz(categoryId, title, 6); // a changer par l'utilisateur courant !!
+    const quizId = quiz[0].quiz_id;
+    if (!quizId) {
+      return res.status(400).send('Erreur lors de l’enregistrement du quiz');
     }
-    // add questions to the question table :
-    console.log('la longueur de questions : ', questions.length);
-    for (let index = 0; index < questions.length; index++) {
-      console.log('the question is : ', questions[index][0]);
-      const question = await pool.query(
-        'INSERT INTO project.questions (quiz_id, question) VALUES ($1, $2) RETURNING  question_id ',
-        [quizId, questions[index][0]],
-      );
-      if (question.rowCount <= 0) {
-        res.status(400).send('Erreur lors de l’enregistrement de la question');
-      }
-      const questionId = question.rows[0].question_id;
-      // add the answers to the question table :
-      for (let j = 1; j <= 4; j++) {
-        if (j === 1) {
-          const answer = await pool.query(
-            'INSERT INTO project.answers (answer, question, is_correct) VALUES ($1, $2, $3) RETURNING  answer ',
-            [questions[index][j], questionId, 1],
-          );
-          if (answer.rowCount <= 0) {
-            res.status(400).send('Erreur lors de l’enregistrement d une reponse');
-          }
-        } else {
-          const answer = await pool.query(
-            'INSERT INTO project.answers (answer, question, is_correct) VALUES ($1, $2, $3) RETURNING  answer ',
-            [questions[index][j], questionId, 0],
-          );
-          if (answer.rowCount <= 0) {
-            res.status(400).send('Erreur lors de l’enregistrement d une reponse');
-          }
-        }
-      }
+    console.log('Quiz created with ID:', quizId);
+    const questionsAnswers = await addQuestionsAnswers(questions, quizId);
+    if (!questionsAnswers) {
+      return res.status(400).send('Erreur lors de l’enregistrement des questions-réponses');
     }
-    return res.status(201).json(quiz.rows[0]);
+    return res.status(201).json(quiz[0]);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Erreur serveur');
   }
 });
 
-// eslint-disable-next-line consistent-return
+/**
+ * Return all the catgories of the database
+ */
 router.get('/categories', async (req, res) => {
   try {
-    const categories = await pool.query('SELECT * FROM project.categories');
-    if (categories.rows.length > 0) {
-      console.log('categories ok');
-      return res.json(categories.rows);
+    const categories = await readAllCategories();
+    if (categories !== undefined) {
+      console.log('categories : ', categories);
+      return res.json(categories);
     }
-    return res.sendStatus(400);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Erreur serveur');
-  }
-});
-
-// Read all quizzes by user_id
-router.get('/:id_user', async (req, res) => {
-  const userId = req.params.id_user;
-  try {
-    const quizzes = await pool.query('SELECT * FROM project.quizzes WHERE user_id=$1', [
-      userId, // a remplcer par l'id de l'user (session)
-    ]);
-    if (quizzes.rows.length > 0) {
-      console.log('quizzes ok');
-      return res.json(quizzes.rows);
-    }
-    return res.sendStatus(400);
+    return res.sendStatus(404);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Erreur serveur');
