0157b8f810e2f3253ce63f6242f0128164af3861
Eda Copur <eda.copur@student.vinci.be>
2023-11-28 10:22:23 +0100

    add quizzes.sj to models

--- api/models/quizzes.js
+++ api/models/quizzes.js
@@ -5,7 +5,7 @@ const pool = require('../db');
 async function readAllQuizzesByUser(userId) {
   const quizzes = await pool.query('SELECT * FROM project.quizzes WHERE user_id=$1', [userId]);
   if (quizzes.rows.length > 0) {
-    console.log('quizzes ok');
+    // console.log('quizzes ok');
     return quizzes.rows;
   }
   return undefined;
