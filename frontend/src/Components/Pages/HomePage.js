e9954ee4e3b47bb1f7bd65a793f0f2d099c42965
Eda Copur <eda.copur@student.vinci.be>
2023-12-11 23:24:01 +0100

    homepage responsive

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -11,24 +11,23 @@ const HomePage = () => {
   const main = document.querySelector('main');
   main.innerHTML = `
   <section id="SectionMain">
-
-        <section class="banner pt-5 d-flex justify-content-center align-items-center ">
-          <div class="container my-5 py-5">
-            <div class="row">
-            <div class="col-md-12 col-12 col-lg-6 text-center d-flex align-items-center">
-                <h1  >
-                Prêt à explorer l'univers des quiz ? Découvrez une multitude de catégories et mettez vos connaissances à l'épreuve !
-                <div>
+    <section class="banner pt-5 d-flex justify-content-center align-items-center ">
+      <div class="container my-5 py-5">
+        <div class="row">
+          <div class="col-12 col-md-6 text-center d-flex align-items-center">
+            <h1 class="text-area">
+              Prêt à explorer l'univers des quiz ? Découvrez une multitude de catégories et mettez vos connaissances à l'épreuve !
+              <div>
                 <a class="btn btn-style" href="/categories" data-uri="">Explorer les catégories</a> <!--href ou data-uri????-->
-                </div>
-                </h1>
-                </div>
-                <div class="col-12 col-lg-6 col-md-12">
-                <img  class="img-fluid" src="${image1}" alt="image banner">
-                </div>
-                </div>
+              </div>
+            </h1>
           </div>
-        </section>
+        <div class="col-12 col-lg-6 col-md-12">
+          <img  class="img-fluid" src="${image1}" alt="image banner">
+        </div>
+      </div>
+    </div>
+  </section>
       
         <svg style="background color:#D9EEE1;" width="100%" height="70" viewBox="0 0 100 100"' preserveAspectRatio="none"> 
         <path id="wavepath" d="M0, 0 L110, 0C35,150 35,0 0,100z" fill="#F4EEFF"></path>
@@ -38,7 +37,7 @@ const HomePage = () => {
         <div class="container my-5 py-5">
           <div class="row">
           <div class="col-12 col-lg-6 col-md-12">
-          <h1> <img class="img-fluid"src="${image2}" alt="image banner"> <h1> 
+          <h1> <img class="img-fluid"src="${image2}" alt="image banner"> </h1> 
             </div>
             <div class="col-md-12 col-12 col-lg-6 text-center d-flex align-items-center">
             <h3>
